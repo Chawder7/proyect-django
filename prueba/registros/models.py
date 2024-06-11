9480a76b946c34a62b2654802b6fb952e1071547
AlexHF7 <chawder.hf00@gmail.com>
2024-06-11 12:27:04 -0600

    inicio app registros

---
+++ prueba/registros/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Alumnos(models.Model):
+    matricula = models.CharField(max_length=12)
+    nombre = models.TextField()
+    carrera = models.TextField()
+    turno = models.CharField(max_length=10)
+    created = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now_add=True)
